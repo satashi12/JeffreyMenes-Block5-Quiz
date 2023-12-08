94b9d8ba5714b49c41e6b5706b17dd29558cf112
satashi12 <jeffreymenes30.com>
2023-12-08 10:07:03 +0800

    1st commit

---
+++ app/src/main/java/com/jeffrey/menes/block5/p1/quiz/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.jeffrey.menes.block5.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun addingtwonumbers()
+    {
+        var num1 = findViewById<EditText>(R.id.number1)
+        var num2 = findViewById<EditText>(R.id.number2)
+        var button1 =  findViewById<Button>(R.id.add_button)
+        var clearbutton =  findViewById<Button>(R.id.clearbutton)
+
+
+
+
+
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JeffreyMenes-Block5-Quiz"
+include(":app")
+ 

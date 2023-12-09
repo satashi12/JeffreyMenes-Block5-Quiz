6ed648f096d0bc0c0e4aec51c6585b3d5e677efd
satashi12 <jeffreymenes30.com>
2023-12-09 19:20:45 +0800

    2nd commit

--- app/src/main/java/com/jeffrey/menes/block5/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/jeffrey/menes/block5/p1/quiz/MainActivity.kt
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addingtwonumbers()
     }
 
     fun addingtwonumbers()
@@ -17,6 +19,11 @@ class MainActivity : AppCompatActivity() {
         var num2 = findViewById<EditText>(R.id.number2)
         var button1 =  findViewById<Button>(R.id.add_button)
         var clearbutton =  findViewById<Button>(R.id.clearbutton)
+
+        button1.setOnClickListener{
+
+            Toast.makeText(this,"Hello, this button is working",Toast.LENGTH_SHORT).show()
+        }
 
 
 
